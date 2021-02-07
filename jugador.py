13d607ee2db3d3c3ff40c9bbe7ea59626855d123
Fernan259 <78517554+Fernan259@users.noreply.github.com>
2021-02-07 17:00:29 -0500

    eqwe

--- jugador.py
+++ jugador.py
@@ -5,12 +5,18 @@ class Jugador(pygame.sprite.Sprite):
     #awqa
     def __init__(self):
         super().__init__()
-        self.Caminando1 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD1.png")
-        self.Caminando2 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD2.png")
-        self.Caminando3 = pygame.image.load("sprites/OsoCaminando3.png")
-        self.Caminando4 = pygame.image.load("sprites/OsoCaminando4.png")
-        self.Caminando5 = pygame.image.load("sprites/OsoCaminando5.png")
-        self.Caminando6 = pygame.image.load("sprites/OsoCaminando6.png")
+        self.CD1 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD1.png")
+        self.CD2 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD2.png")
+        self.CD3 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD3.png")
+        self.CD4 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD4.png")
+        self.CD5 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD5.png")
+        self.CD6 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD6.png")
+        self.CD7 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD7.png")
+        self.CD8 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD8.png")
+        self.CD9 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD9.png")
+        self.CD10 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD10.png")
+        self.CD11 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD11.png")
+        self.CD12 = pygame.image.load("sprites/SpriteMujer1/CaminandoDerecha/CD12.png")
         self.Caminando1v2 = pygame.image.load("sprites/OsoCaminando1.2.png")
         self.Caminando2v2 = pygame.image.load("sprites/OsoCaminando2.1.png")
         self.Caminando3v2 = pygame.image.load("sprites/OsoCaminando3.1.png")
@@ -18,8 +24,7 @@ class Jugador(pygame.sprite.Sprite):
         self.Caminando5v2 = pygame.image.load("sprites/OsoCaminando5.1.png")
         self.Caminando6v2 = pygame.image.load("sprites/OsoCaminando6.1.png")
 
-        self.listaSpritesOsoCaminando = [self.Caminando1, self.Caminando2, self.Caminando3, self.Caminando4,
-                                           self.Caminando5, self.Caminando6]
+        self.listaSpritesOsoCaminando = [self.CD1,self.CD2,self.CD3,self.CD4,self.CD5,self.CD6,self.CD7,self.CD8,self.CD9,self.CD10,self.CD11,self.CD12]
         self.posimagen = 0
         self.imagenosocaminando = self.listaSpritesOsoCaminando[self.posimagen]
         self.rect = self.imagenosocaminando.get_rect()
@@ -31,7 +36,7 @@ class Jugador(pygame.sprite.Sprite):
                                            self.Caminando5v2, self.Caminando6v2]
         self.imagenosocaminandov2 = self.listaSpritesOsoCaminandov2[self.posimagen]
 
-        self.imageninicial = self.Caminando1
+        self.imageninicial = self.CD1
 
     def oso(self, ventana, posicionix, posicioniy ):
         pygame.event.pump()
